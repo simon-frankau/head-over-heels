28d7c26ab835d1b704e40c73f1b3d41bebb4931f
Simon Frankau <sgf@arbitrary.name>
2017-06-27 16:15:02 +0100

    Tidy up sound48k.asm, around interrupt-driven sound

--- src/sound/sound48k.asm
+++ src/sound/sound48k.asm
@@ -1,39 +1,43 @@
-	;;
-	;; sound48k.asm
-	;;
-	;; 48K sound production
-	;;
+;;
+;; sound48k.asm
+;;
+;; 48K sound production
+;;
 
-	;; Main exported functions:
-	;; * AltPlaySound
-	;; * IrqFn
-	;; * PlaySound
+;; Main exported functions:
+;; * AltPlaySound
+;; * IrqFn
+;; * PlaySound
 
 	;; FIXME: Needs a good, old tidy-up.
 	
 SndCount:	DEFB $00
-Snd2:		DEFB $FF
+Snd2:		DEFB $FF        ; TODO: $FF = end of sound?
 L964C:		DEFB $00
 L964D:		DEFB $00
 SndEnable:	DEFB $80	; Top bit set if sound enabled.
 
-        ;; This is patched by the 128K patch
+;; (Overwritten by 128K patch)
 AltPlaySound:	LD	A,(Snd2)
 		CP	$00
 		RET	Z
 		LD	B,$C3
 		JP	PlaySound
 
-        ;; This is patched by the 128K patch
-IrqFn:		JP	IrqFnCore
+;; (Overwritten by 128K patch)
+IrqFn:          JP      SndHandler
 
-        ;; Blatted by 128K patch
-	;; Ratio between elements are twelth root of two - definitions for notes in a scale.
-ScaleTable:	DEFW 1316,1241,1171,1105,1042,983,927,875,825,778,734,692
+;; (Overwritten by 128K patch)
+;; Ratio between elements are twelth root of two - definitions for
+;; notes in a scale.
+ScaleTable:     DEFW 1316,1241,1171,1105,1042,983,927,875,825,778,734,692
 
         ;; This is patched by 128K patch
 	;; FIXME: Called from everywhere.
 	;; Called with B containing the sound id.
+        
+;; May play the sound either immediately or using the interrupt-driven
+;; mechanism.
 PlaySound:	LD	A,(SndEnable)
 		RLA
 		RET	NC		; Exit early if sound disabled.
@@ -105,6 +109,7 @@ PS_6:		LD	E,C
 		LD	C,A
 		AND	$04
 		JR	NZ,PS_8
+        ;; Interrupt-driven sound-playing.
 		LD	A,C
 		AND	$02
 		LD	B,$0A
@@ -113,13 +118,16 @@ PS_6:		LD	E,C
 		RR	C
 		JR	C,PS_7
 		LD	B,$02
+        ;; TODO: SndCtrl is 8|2, 80|10|2 or 2...
 	;; Scribble over locations the interrupt handler cares about.
 	;; So, disable interrupts.
 PS_7:		DI
+        ;; Copy SndCount into Snd2.
 		LD	HL,SndCount
 		LD	A,(HL)
 		INC	HL
 		LD	(HL),A
+        ;; Then write the main sound-triggering variables
 		LD	HL,SndCtrl
 		LD	(HL),B 		; Write SndCtrl
 		INC	HL
@@ -132,6 +140,7 @@ PS_7:		DI
 		LD	(HL),A		; Clear ScoreIdx
 		EI
 		RET
+        ;; Immediate sound-playing case.
 PS_8:		EX	DE,HL
 		LD	B,(HL)
 		INC	HL
@@ -153,257 +162,329 @@ PS_9:		XOR	A
 		LD	(SndThing),A
 		RET
 
-	;;  Core interrupt handler.
-IrqFnCore:	LD	IY,SndCtrl
-		LD	A,(Snd2)
-		INC	A
-		RET	Z
-		LD	IX,(ScorePtr)
-		LD	DE,(ScoreIdx)
-		LD	D,$00
-		ADD	IX,DE	   ; Generate current score pointer, put in IX.
-		BIT	1,(IY+$00) ; SndCtrl
-		JR	NZ,IFC_2
-		LD	A,(NoteLen)
-		AND	A
-		JP	NZ,OtherSoundThing
-		RES	4,(IY+$00) ; SndCtrl - turn on glissando
-		CALL	GetScoreByte
-		LD	D,A
-		INC	A
-		JR	NZ,IFC_4
-		CALL	GetScoreByte
-		CP	D
-		JR	NZ,IFC_1
-		LD	(Snd2),A
-		RET
-IFC_1:		AND	A
-		JR	NZ,IFC_2
-		LD	(ScoreIdx),A
-		LD	IX,(ScorePtr)
-IFC_2:		LD	D,(IX+$00)
-		CALL	UnpackD
-		LD	(IY+$05),D ; Something
-		LD	B,$08
-		LD	A,E
-		AND	$02
-		JR	NZ,IFC_3
-		LD	B,$80
-		RRC	E
-		JR	C,IFC_3
-		LD	B,$00
-IFC_3:		LD	A,(SndCtrl)
-		AND	$02
-		OR	B
-		LD	E,A
-		AND	$02
-		RLA
-		RLA
-		RLA
-		OR	E
-		LD	(SndCtrl),A
-		CALL	GetScoreByte
-		LD	D,A
-IFC_4:		LD	A,(SndCtrl)
-		AND	$F9
-		LD	(SndCtrl),A 	; Turn off 0x2 and 0x4
-		CALL	UnpackD
-		LD	C,D
-		LD	D,$00
-		LD	HL,IrqArray
-		ADD	HL,DE
-		LD	A,(HL)
-		LD	(NoteLen),A
-		LD	A,C
-		AND	A
-		JR	NZ,IrqBits	; Tail call
-		SET	2,(IY+$00)	; SndCtrl
-		RET
+;; Sound interrupt handler.
+SndHandler:     LD      IY,SndCtrl
+        ;; If Snd2 == $FF, nothing to do.
+                LD      A,(Snd2)
+                INC     A
+                RET     Z
+        ;; Generate the current score pointer, put it in IX.
+                LD      IX,(ScorePtr)
+                LD      DE,(ScoreIdx)
+                LD      D,$00
+                ADD     IX,DE
+        ;; If $02 bit of SndCtrl is set, start the score
+                BIT     1,(IY+SND_CTRL)
+                JR      NZ,PlayNextPhrase
+        ;; If the note hasn't finished, play more of it.
+                LD      A,(NoteLen)
+                AND     A
+                JP      NZ,DoNoteCont
+        ;; If note length has reached zero, at which point we pick
+        ;; up the next note.
+                RES     4,(IY+SND_CTRL) ; Disable 'glissando complete'
+                CALL    GetScoreByte
+                LD      D,A
+                INC     A
+                JR      NZ,PlayNextNote ; Usual next-note case.
+        ;; If next byte is $FF, it's a special case:
+                CALL    GetScoreByte
+                CP      D
+                JR      NZ,SH_1
+        ;; Following byte also $FF? End of score. Set Snd2 to $FF and return.
+                LD      (Snd2),A
+                RET
+SH_1:           AND     A
+                JR      NZ,PlayNextPhrase
+        ;; Follwing byte was $00. Loop to start of score.
+                LD      (ScoreIdx),A
+                LD      IX,(ScorePtr)
+        ;; NB: Fall through.
 
-GetScoreByte:	INC	IX		; Contains ScorePtr + ScoreIdx
-		INC	(IY+$04)	; ScoreIdx
-		LD	A,(IX+$00)
-		RET
+;; Read the next byte and start playing...
+PlayNextPhrase: LD      D,(IX+$00)
+                CALL    UnpackD
+        ;; Load an absolute value for the note.
+                LD      (IY+SND_NOTE),D
+        ;; Cases for E & $03:
+        ;; 0: B = $00 - usual.
+        ;; 1: B = $80 - glissando.
+        ;; 2: B = $08 - silence.
+                LD      B,$08
+                LD      A,E
+                AND     $02
+                JR      NZ,PNP_2
+                LD      B,$80
+                RRC     E
+                JR      C,PNP_2
+                LD      B,$00
+        ;; Set up the control flags for the new phrase...
+PNP_2:          LD      A,(SndCtrl)
+                AND     $02             ; Only keep 'new score' flag.
+                OR      B
+                LD      E,A
+        ; Copy $02 flag to $10 - no glissando at start of score.
+                AND     $02
+                RLA
+                RLA
+                RLA
+        ;; And add in the new phrase control flags.
+                OR      E
+                LD      (SndCtrl),A
+        ;; And now get the first byte to play...
+                CALL    GetScoreByte
+                LD      D,A
+        ;; There's a score byte in D.
 
-	;; More interrupt-related stuff...
-IrqBits:	ADD	A,(IY+$05) 	; Something
-	;; Divide A by 12, result plus one in C, remainder in A.
-		LD	B,12
-		LD	H,0
-		LD	C,H
-IB_0:		INC	C
-		SUB	B
-		JR	NC,IB_0
-		ADD	A,B
-	;; Look up the basic delay constant in the scale table.
-		LD	L,A
-		ADD	HL,HL
-		LD	DE,ScaleTable
-		ADD	HL,DE
-		LD	E,(HL)
-		INC	HL
-		LD	D,(HL)
-		INC	HL	; (Seems unnecessary??)
-	;; Generate the octave constant in A.
-		LD	B,C
-		LD	A,$02
-IB_1:		RLCA
-		DJNZ	IB_1
-		BIT	4,(IY+$00) 	; SndCtrl - get glissando bit
-		JR	NZ,IB_2
-	;; Glissando case: Double the constant and add 8. Don't ask me.
-		RLCA			; Glissando case...
-		ADD	A,$08
-	;; Otherwise, don't.
-IB_2:		LD	(SoundLenConst),A
-	;; Now shift the delay constant (in DE), according to the octave we're in.
-		LD	B,C
-IB_3:		SRL	D
-		RR	E
-		DJNZ	IB_3
-	;; Now some octave-specific cases:
-		LD	A,C
-		DEC	A
-		JR	Z,IB_5	; Go to IB_5 if bottom octave/octave 1 (encoded as 0).
-		LD	B,$09
-		LD	A,$04
-		SUB	C
-		JR	C,IB_4	; Go to IB_4 if octave > 4
-		RLA
-		NEG
-		ADD	A,B
-		LD	B,A
-IB_4:		LD	A,E
-		SUB	B
-		LD	E,A
-		JR	NC,IB_5
-		DEC	D
-	;; Check for glissando
-IB_5:		LD	A,(SndCtrl)
-		AND	$90
-		CP	$80
-		JR	NZ,IB_8
-	;; Glissando case
-		LD	(SoundDelayTarget),DE
-		LD	HL,(SoundDelayConst)
-		EX	DE,HL
-		XOR	A
-		SBC	HL,DE		; Now contains difference between current and target.
-		LD	A,(NoteLen)
-	;; Count significant bits in A.
-		LD	B,$08
-IB_6:		RLA
-		DEC	B
-		JR	NC,IB_6
-	;; Divide through by that.
-IB_7:		SRA	H
-		RR	L
-		DJNZ	IB_7
-	;; And that gives us our delta to finish in the appropriate timeframe.
-		LD	(SoundDelayDelta),HL
-		RES	4,(IY+$00) 	; SndCtrl - enable glissando.
-		JR	DoCurrSound	; Tail call
-	;; Non-glissando case
-IB_8:		LD	(SoundDelayConst),DE
-	;; NB: Fall through
+;; Play the next note.
+PlayNextNote:   LD      A,(SndCtrl)
+                AND     ~$06
+                LD      (SndCtrl),A     ; Turn off new phrase and rest flags
+        ;; Unpack the score byte to length and pitch delta from previous...
+                CALL    UnpackD
+                LD      C,D
+                LD      D,$00
+                LD      HL,NoteLens
+                ADD     HL,DE
+                LD      A,(HL)
+                LD      (NoteLen),A
+                LD      A,C
+                AND     A
+        ;; then play it.
+                JR      NZ,DoNote       ; Tail call
+                SET     2,(IY+SND_CTRL) ; Set rest (silence) if pitch was 0.
+                RET
 
-DoCurrSound:	LD	B,(IY+$0A)	; SoundLenConst
-		LD	DE,(SoundDelayConst)
-		LD	A,(SndThing)
-		INC	A
-		RET	NZ
-	;; Fall through!
+;; Update both the in-register pointer, and the index in memory, and
+;; fetch the next byte of score (in A).
+GetScoreByte:   INC     IX              ; Contains ScorePtr + ScoreIdx
+                INC     (IY+SCORE_IDX)
+                LD      A,(IX+$00)
+                RET
 
-	;; Writes out B edges with a delay constant of DE.
-DoSound:	LD	A,E
-	;; Bottom two bits control the overwrite of the jump to DS_1 -
-	;; back jump to the OUT, or one of the INC HLs. 
-		AND	$03
-		NEG
-		ADD	A,$F0
-		LD	(DS_3+1),A
-	;; Divide DE by 4.
-		SRL	D
-		RR	E
-		SRL	D
-		RR	E
-	;; Load up previously written value...
-		LD	A,(LastOut)
-	;; Jump target area... these are simply delay ops.
-		INC	HL
-		INC	HL
-		INC	HL
-	;; Write/toggle the sound bit.
-DS_1:		OUT	($FE),A
-		XOR	$30
-	;; Delay loop.
-		LD	C,A
-		PUSH	DE
-DS_2:		DEC	DE
-		LD	A,D
-		OR	E
-		JP	NZ,DS_2
-		POP	DE
-		LD	A,C
-	;; And loop.			
-DS_3:		DJNZ	DS_1		; Target of self-modifying code!
-		RET
+;; Play a new note, specified in Sound Note.
+;; Note is passed in in A, as an offset from previous note.
+DoNote:         ADD     A,(IY+SND_NOTE)
+        ;; Divide A by 12 to get octave and note:
+        ;; Result plus one in C (octave), remainder in A (note in scale).
+                LD      B,12
+                LD      H,0
+                LD      C,H
+DN_0:           INC     C
+                SUB     B
+                JR      NC,DN_0
+                ADD     A,B
+        ;; Look up the delay constant in the scale table, put in DE.
+                LD      L,A
+                ADD     HL,HL
+                LD      DE,ScaleTable
+                ADD     HL,DE
+                LD      E,(HL)
+                INC     HL
+                LD      D,(HL)
+                INC     HL              ; (Seems unnecessary??)
+        ;; Calculate the sound length:
+        ;; Base it on the octave - each octave makes the note last half as
+        ;; many cycles, so we'll double the number of cycles to keep the
+        ;; note length more-or-less constant.
+                LD      B,C
+                LD      A,$02
+DN_1:           RLCA
+                DJNZ    DN_1
+        ;; If 'glissando complete' is not set, make the note run a bit longer:
+                BIT     4,(IY+SND_CTRL)
+                JR      NZ,DN_2
+        ;; Double the constant and add 8.
+                RLCA
+                ADD     A,$08
+        ;; Then store it in SndLen.
+DN_2:           LD      (SndLen),A
+        ;; Now shift the delay constant (in DE), according to the
+        ;; octave we're in, to get a note pitched to the right octave.
+                LD      B,C
+DN_3:           SRL     D
+                RR      E
+                DJNZ    DN_3
+        ;; Now some octave-specific cases:
+        ;; I assume this is a minor tweak to improve the tuning.
+                LD      A,C
+                DEC     A
+                JR      Z,DN_5  ; No adjustment for bottom octave (C == 1)
+                LD      B,$09
+                LD      A,$04
+                SUB     C
+                JR      C,DN_4  ; B = 9 for octave > 4
+                RLA             ; A = 2 * (4 - C)
+                NEG             ; A = 2 * C - 8
+                ADD     A,B     ; A = 2 * C + 1
+                LD      B,A     ; B = 2 * C + 1
+DN_4:           LD      A,E     ; And then subtract B from DE
+                SUB     B
+                LD      E,A
+                JR      NC,DN_5
+                DEC     D
+        ;; Check for glissando
+DN_5:           LD      A,(SndCtrl)
+                AND     $90
+                CP      $80     ; Need glissando set, glissando not complete.
+                JR      NZ,DN_8
+        ;; Glissando case
+                LD      (SndWavelenTgt),DE
+                LD      HL,(SndWavelen)
+                EX      DE,HL
+                XOR     A
+                SBC     HL,DE   ; Now contains difference between current and target.
+                LD      A,(NoteLen)
+        ;; Count significant bits in A.
+                LD      B,$08
+DN_6:           RLA
+                DEC     B
+                JR      NC,DN_6
+        ;; Divide through by that.
+DN_7:           SRA     H
+                RR      L
+                DJNZ    DN_7
+        ;; And that gives us our delta to finish in the appropriate timeframe.
+                LD      (SndWavelenDelta),HL
+                RES     4,(IY+SND_CTRL) ; Glissando not complete.
+                JR      DoCurrSound     ; Tail call
+        ;; Non-glissando case
+DN_8:           LD      (SndWavelen),DE
+        ;; NB: Fall through
 
-OtherSoundThing:LD	A,(SndCtrl)
-		LD	B,A
-		DEC	(IY+$01)	; NoteLen
-		JR	NZ,OST_1
-		AND	$80
-		RET	Z		; Do nothing if NoteLen is zero, and top
-					; bit of SndCtrl is reset.
-		LD	A,B
-OST_1:		AND	$0C
-		RET	NZ		; Return if 0x8 or 0x4 bit set in SndCtrl
-		LD	A,B
-		AND	$90
-		CP	$80
-		JR	NZ,DoCurrSound  ; Skip if IrqFlag & 0x90 != 0x80
-		LD	HL,(SoundDelayConst) ; Add SoundDelayDelta to SoundDelayConst
-		LD	DE,(SoundDelayDelta)
-		ADD	HL,DE
-		LD	E,L
-		LD	D,H
-		LD	BC,(SoundDelayTarget)	; Check if we've hit the limit of the glissando.
-		XOR	A
-		SBC	HL,BC
-		RRA
-		XOR	A,(IY+$0B)	; SoundDelayDelta high byte - this bit is for signed check.
-		RLA
-		JR	C,OST_2
-		SET	4,(IY+$00)	; Set 0x10 bit of SndCtrl - turn off glissando.
-		LD	DE,(SoundDelayTarget) ; And use the target frequency
-OST_2:		LD	(SoundDelayConst),DE
-		JR	DoCurrSound 	; Tail call
+;; Play the sound with the current length (cycle count) and wavelength.
+DoCurrSound:    LD      B,(IY+SND_LEN)
+                LD      DE,(SndWavelen)
+                LD      A,(SndThing)
+                INC     A
+                RET     NZ
+        ;; Fall through!
 
-UnpackD:	LD	BC,$0307
-		LD	A,D
-		AND	C
-		LD	E,A		; Mask bottom 3 bits into E.
-		LD	A,C
-		CPL
-		AND	D
-		LD	D,A		; Mask top 5 bits of D.
-UnpackD_1:	RRC	D
-		DJNZ	UnpackD_1
-		RET			; And rotate into bottom position.
+;; Writes out B edges with a delay constant of DE.
+DoSound:        LD      A,E
+        ;; Bottom two bits control the overwrite of the jump to DS_1 -
+        ;; back jump to the OUT, or one of the INC HLs.
+                AND     $03
+                NEG
+                ADD     A,$F0
+                LD      (DS_3+1),A
+        ;; Divide DE by 4.
+                SRL     D
+                RR      E
+                SRL     D
+                RR      E
+        ;; Load up previously written value...
+                LD      A,(LastOut)
+        ;; Jump target area... these are simply delay ops.
+                INC     HL
+                INC     HL
+                INC     HL
+        ;; Write/toggle the sound bit.
+DS_1:           OUT     ($FE),A
+                XOR     $30
+        ;; Delay loop.
+                LD      C,A
+                PUSH    DE
+DS_2:           DEC     DE
+                LD      A,D
+                OR      E
+                JP      NZ,DS_2
+                POP     DE
+                LD      A,C
+        ;; And loop.
+DS_3:           DJNZ    DS_1            ; Target of self-modifying code!
+                RET
 
-IrqArray:	DEFB $01,$02,$04,$06,$08,$0C,$10,$20
+;; Play an existing note, performing glissando update step etc. as needed.
+DoNoteCont:
+        ; Do nothing if NoteLen is zero, and not glissando.
+                LD      A,(SndCtrl)
+                LD      B,A
+                DEC     (IY+NOTE_LEN)
+                JR      NZ,DNC_1
+                AND     $80
+                RET     Z
+                LD      A,B
+        ;; Silent if 0x8 (silent phrase) or 0x4 (rest) bit set in SndCtrl
+DNC_1:          AND     $0C
+                RET     NZ
+        ;; Just go play sound if not glissando (SndCtrl & 0x90 != 0x80)
+                LD      A,B
+                AND     $90
+                CP      $80
+                JR      NZ,DoCurrSound  ; Jump if not glissando.
+        ;; With glissando, apply delta to wavelength
+                LD      HL,(SndWavelen)
+                LD      DE,(SndWavelenDelta)
+                ADD     HL,DE
+                LD      E,L
+                LD      D,H
+        ;; Now check if we've hit the limit of the glissando:
+                LD      BC,(SndWavelenTgt)
+                XOR     A
+                SBC     HL,BC
+                RRA                     ; Put carry in high bit of A.
+        ;; And compare with sign of SndWavelenDelta high byte - have
+        ;; we gone past in the right direction?
+                XOR     A,(IY+SND_WAVELEN_DELTA)
+                RLA
+                JR      C,DNC_2
+        ;; We have passed the target:
+        ;; Set glissando complete bit...
+                SET     4,(IY+SND_CTRL)
+        ;; And use the target frequency...
+                LD      DE,(SndWavelenTgt)
+        ;; Update SndWavelen and run the sound.
+DNC_2:          LD      (SndWavelen),DE
+                JR      DoCurrSound     ; Tail call
+
+;; Given a byte in D, put the bottom three bits in E, and the next
+;; five in D (rotated to the bottom).
+UnpackD:        LD      BC,$0307
+                LD      A,D
+                AND     C
+                LD      E,A             ; Mask bottom 3 bits into E.
+                LD      A,C
+                CPL
+                AND     D
+                LD      D,A             ; Mask top 5 bits of D.
+UnpackD_1:      RRC     D
+                DJNZ    UnpackD_1
+                RET                     ; And rotate into bottom position.
+
+;; Look-up table of note lengths.
+NoteLens:	DEFB $01,$02,$04,$06,$08,$0C,$10,$20
+
+;; Flags for SndCtrl:
+;; $02 - start score
+;; $04 - is a rest - silence
+;; $08 - silence for whole phrase
+;; $10 - glissando completed
+;; $80 - glissando wanted
 
 SndThing:		DEFB $FF
 SndCtrl:		DEFB $00
 NoteLen:		DEFB $00
 ScorePtr:		DEFW $0000
 ScoreIdx:		DEFB $00
-Something:		DEFB $00
-SoundDelayTarget:	DEFW $0000
-SoundDelayConst:	DEFW $0000
-SoundLenConst:		DEFB $00
-SoundDelayDelta:	DEFW $0000
+SndNote:		DEFB $00         ; Musical scale note number.
+SndWavelenTgt:		DEFW $0000       ; Target wavelen for glissando.
+SndWavelen:		DEFW $0000       ; Delay between edges to play.
+SndLen:			DEFB $00         ; Number of edges to play.
+SndWavelenDelta:	DEFW $0000       ; Delta to apply when glissando-ing.
+
+;; Offsets from SndCtrl, used with IY.
+SND_CTRL:		EQU 0
+NOTE_LEN:		EQU 1
+SCORE_PTR:		EQU 2
+SCORE_IDX:		EQU 4
+SND_NOTE:		EQU 5
+SND_WAVELEN_TGT:	EQU 6
+SND_WAVELENGTH:		EQU 8
+SND_LEN:		EQU 10
+SND_WAVELEN_DELTA:	EQU 11
+        
+        
 	;; FIXME: Some constants seem to overlap with the table?
 SoundTable:		DEFW $98E1,$98DD,$98CB,L9909,L9914
 			DEFW L991F,L9932,L993D,L994A,L994D,L9950,L9958,L996A
